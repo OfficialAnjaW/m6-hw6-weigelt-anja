872dd18e5e374be718bf4fc1c624a307c38667de
Anja Weigelt <aweigelt@ufl.edu>
2019-09-29 16:47:05 -0400

    Add messages to button clicks

--- main.js
+++ main.js
@@ -31,3 +31,36 @@ for(i = 0; i < newButtonColor.length; i++) {
 }
 
 ----------------------------- */
+
+// 2. Box 1 Message
+
+var buttonOne = document.querySelector('.yellow-button');
+var messageOne = document.createTextNode('Oooh - so close, but no cigar!');
+
+function buttonOneMessage() {
+  buttonOne.appendChild(messageOne);
+}
+
+buttonOne.addEventListener('click', buttonOneMessage);
+
+// 3. Box 2 Message
+
+var buttonTwo = document.querySelector('.turquoise-button');
+var messageTwo = document.createTextNode('DING DING DING - We have a winner!');
+
+function buttonTwoMessage() {
+  buttonTwo.appendChild(messageTwo);
+}
+
+buttonTwo.addEventListener('click', buttonTwoMessage);
+
+// 4. Box 3 Message
+
+var buttonThree = document.querySelector('.green-button');
+var messageThree = document.createTextNode('Oops, sorry! Better luck next time!');
+
+function buttonThreeMessage() {
+  buttonThree.appendChild(messageThree);
+}
+
+buttonThree.addEventListener('click', buttonThreeMessage);
